a9ab5a22100637d1a5c665748e25b736950b9ddc
Bmoyal <67309589+Bmoyal@users.noreply.github.com>
2023-03-14 15:58:46 +0200

    updated

--- scraper.py
+++ scraper.py
@@ -47,3 +47,5 @@ def scrape_personal_info():
     # Save the formatted HTML to a file
     with open('index.html', 'w', encoding='utf-8') as f:
         f.write(formatted_html)
+        
+scrape_personal_info()
